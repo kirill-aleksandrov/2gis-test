ad0f726e2e739765e2da6424d6d8c92875023269
Kirill Aleksandrov <xakepgame@gmail.com>
2018-09-03 22:29:08 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './main.css';
+import DG from '2gis-maps';
+
+DG.map('map', {
+  'center': [54.98, 82.89],
+  'zoom': 13
+});
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: [ "style-loader", "css-loader" ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
